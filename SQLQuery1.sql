0ff5e6dbe2da1b9be23f3cf00c84f8e8ad865e66
wild1702 <wild1702@student.nhl.nl>
2018-03-28 14:11:07 +0200

    Update

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Login] (
+    [Username]  VARCHAR (50)  NOT NULL,
+    [Password]  VARCHAR (100) NOT NULL,
+    [Email]     VARCHAR (100) NOT NULL,
+    [ID]        INT           IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_Login] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROC UserAdd
+@u varchar(50),
+@p varchar(100),
+@e varchar(100)
+AS
+INSERT INTO Login (Username, Password, Email)
+VALUES(@u, @p, @e)
